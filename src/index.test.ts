eefa076dbe1ad6a8df47f7dc2b7071d6123285dd
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-03-31 19:49:31 -0400

    build: :wrench: Initialize TS Playground repository config

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { test } from './index';
+
+describe('index', () => {
+  describe('test', () => {
+    it('should return true', () => {
+      expect(test()).toBe(true);
+    });
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @function test
+ * @description A simple test function that returns true
+ * @returns {boolean} Always returns true
+ */
+export function test(): boolean {
+  return true;
+}
